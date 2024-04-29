1440c29e1993770adec8ca9eb0440ac2503766c9
wdmhere <wdmhere@outlook.com>
2024-04-29 11:27:01 +0800

    搭建空项目

---
+++ src/main/java/com/china/hummingbirdq/HummingbirdQApplication.java
@@ -0,0 +1,13 @@
+package com.china.hummingbirdq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HummingbirdQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HummingbirdQApplication.class, args);
+    }
+
+}
